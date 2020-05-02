4d8b46cbbbcd21eb6f91d714037717a12bf85e0c
sherl <sherlockeria@gmail.com>
2020-05-02 23:53:48 +0700

    jwt auth

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
 exports.login = async (req, res) => {
     const email = req.body.email || '';
     const password = req.body.password || '';
@@ -13,8 +16,17 @@ exports.login = async (req, res) => {
 
     if ( user ) {
         if (bcrypt.compareSync(password, user.password)) {
+            const payload = {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                created_at: user.created_at,
+                updated_at: user.updated_at
+            };
+            const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET);
             res.send({
-                status: 'success'
+                status: 'success',
+                token: token
             });
         } else {
             res.status(400).send({
@@ -28,4 +40,11 @@ exports.login = async (req, res) => {
             message: 'Email not found'
         });
     }
+};
+
+exports.me = (req, res) => {
+    res.send({
+        status: 'success',
+        data: req.authUser
+    });
 };
--- app/routers/auth.js
+++ app/routers/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const jwtAuth = require('../middlewares/jwt-auth');
 
 const authController = require('../controllers/auth');
 
 const router = express.Router();
 
 router.post('/login', authController.login);
+router.get('/me', jwtAuth);
 
 module.exports = router;
