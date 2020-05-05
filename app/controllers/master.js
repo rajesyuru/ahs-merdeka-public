4b0a639f09ac0091a796a8cd17d1eb349085c5e3
sherl <sherlockeria@gmail.com>
2020-05-05 23:10:33 +0700

    users

--- app/controllers/master.js
+++ app/controllers/master.js
@@ -1,4 +1,4 @@
-const {Group, Op} = require('../models');
+const {Group, Op, User} = require('../models');
 
 exports.groups = async (req, res) => {
     const page = req.query.page * 1 || 1;
@@ -31,3 +31,47 @@ exports.groups = async (req, res) => {
         data: groups,
     });
 };
+
+exports.users = async (req, res) => {
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 20;
+    const search = req.query.search || '';
+    const offset = (page - 1) * limit;
+
+    const count = await User.count({
+        where: {
+            name: {
+                [Op.iLike]: `%${search}%`,
+            },
+        },
+    });
+    const users = await User.findAll({
+        attributes: [
+            'name',
+            'email',
+            'group_id',
+            'merchant_id',
+            'created_at',
+            'updated_at'
+        ],
+        where: {
+            name: {
+                [Op.iLike]: `%${search}%`,
+            },
+        },
+        include: ['group', 'merchant'],
+        order: [
+            ['created_at', 'asc']
+        ],
+        limit: limit,
+        offset: offset,
+    });
+
+    res.send({
+        status: 'success',
+        totalData: count,
+        totalPage: Math.ceil(count / limit),
+        page: page,
+        data: users
+    });
+}
--- app/routers/master.js
+++ app/routers/master.js
@@ -1,9 +1,26 @@
 const express = require('express');
 
+const {jwtAuth} = require('../mwares/jwt-auth');
+const {canAccess} = require('../permissions/merchant');
+
 const masterController = require('../controllers/master');
 
 const router = express.Router();
 
+router.use(jwtAuth);
+
+// make sure user can access
+router.use((req, res, next) => {
+    console.log(req.authUser);
+    if (!canAccess(req.authUser)) {
+        res.status(403);
+        return res.send('Forbidden');
+    }
+
+    next();
+});
+
 router.get('/groups', masterController.groups);
+router.get('/users', masterController.users);
 
 module.exports = router;
