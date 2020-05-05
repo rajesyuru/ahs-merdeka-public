24aab73e46d92dc25f5d8425a2743ee57d65a8ff
sherl <sherlockeria@gmail.com>
2020-05-05 23:24:28 +0700

    group

--- app/routers/master.js
+++ app/routers/master.js
@@ -1,24 +1,8 @@
 const express = require('express');
 
-const {jwtAuth} = require('../mwares/jwt-auth');
-const {canAccess} = require('../permissions/merchant');
-
 const masterController = require('../controllers/master');
 
 const router = express.Router();
-
-router.use(jwtAuth);
-
-// make sure user can access
-router.use((req, res, next) => {
-    console.log(req.authUser);
-    if (!canAccess(req.authUser)) {
-        res.status(403);
-        return res.send('Forbidden');
-    }
-
-    next();
-});
 
 router.get('/groups', masterController.groups);
 
